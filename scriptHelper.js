64e282d582be38ee7407e4c126c39a8fbd44ce7e
Zachary <gray.zachary1994@gmail.com>
2022-12-08 14:07:34 -0600

    Trying to make small changes to pass some autograder.

--- scriptHelper.js
+++ scriptHelper.js
@@ -35,10 +35,10 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-    let pilotNameInput = document.querySelector("input[name=pilotName");
-    let copilotNameInput = document.querySelector("input[name=copilotName]");
-    let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
-    let cargoMassInput = document.querySelector("input[name=cargoMass]");
+    let pilotName = document.querySelector("input[name=pilotName");
+    let copilotName = document.querySelector("input[name=copilotName]");
+    let fuelLevel = document.querySelector("input[name=fuelLevel]");
+    let cargoMass = document.querySelector("input[name=cargoMass]");
     let pilotStatus = document.getElementById("pilotStatus");
     let copilotStatus = document.getElementById("copilotStatus");
     let fuelStatus = document.getElementById("fuelStatus");
@@ -49,13 +49,14 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     pilotStatus.innerHTML = `Pilot ${pilot} is ready for launch`;
     copilotStatus.innerHTML = `Co-pilot ${copilot} is ready for launch`;
 
-    if(fuelLevelInput.value<10000){
+    if(fuelLevel.value<10000){
         fuelStatus.innerHTML = "Fuel level too low for launch"
+        cargoStatus.innerHTML = "Cargo mass low enough for launch"
         launchStatus.innerHTML = "Shuttle Not Ready for Launch"
         launchStatus.style.color = "rgb(199, 37, 78)";
         list.style.visibility = "visible";
         return;
-    } else if(cargoMassInput.value>10000){
+    } else if(cargoMass.value>10000){
         fuelStatus.innerHTML = "Fuel level high enough for launch"
         cargoStatus.innerHTML = "Cargo mass too heavy for launch"
         launchStatus.innerHTML = "Shuttle Not Ready for Launch"
